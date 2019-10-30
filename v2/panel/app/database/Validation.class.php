9e69b17bb2588830016357dffc1396ab6da981d5
Leonardo Mauricio <leomauricio7@gmail.com>
2019-10-30 12:49:34 -0300

    modulo de segui usuario

--- v2/panel/app/database/Validation.class.php
+++ v2/panel/app/database/Validation.class.php
@@ -1,34 +1,40 @@
 <?php
 
-class Validation extends Conn {
+class Validation extends Conn
+{
 
     private $login;
     private $senha;
 
-    public function setLogin($login) {
+    public function setLogin($login)
+    {
         $this->login = $login;
     }
 
-    public function setSenha($senha) {
+    public function setSenha($senha)
+    {
         $this->senha = $senha;
     }
 
-    public function getLogin() {
+    public function getLogin()
+    {
         return $this->login;
     }
 
-    public function getSenha() {
+    public function getSenha()
+    {
         return $this->senha;
     }
 
-    public function logar() {
+    public function logar()
+    {
         $pdo = parent::getConn();
 
         $logar = $pdo->prepare("SELECT * FROM usuarios WHERE email = ? AND senha = ?");
         $logar->bindValue(1, $this->getLogin());
         $logar->bindValue(2, $this->getSenha());
         $logar->execute();
-        if ($logar->rowCount() == 1):
+        if ($logar->rowCount() == 1) :
             $dados = $logar->fetch(PDO::FETCH_ASSOC);
             $_SESSION['userId'] = $dados['id'];
             $_SESSION['user'] = $dados['nome'];
@@ -38,10 +44,10 @@ class Validation extends Conn {
             $_SESSION['avatar'] = $dados['avatar'];
             $_SESSION['logado'] = true;
             $_SESSION["sessiontime"] = time() + 60 * 00;
-            return true; 
-        else:
-            $_SESSION['erro'] = 
-            '<div class="alert alert-danger alert-dismissible" role="alert">
+            return true;
+        else :
+            $_SESSION['erro'] =
+                '<div class="alert alert-danger alert-dismissible" role="alert">
                 <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                 <h5 align="center"><i class="fas fa-exclamation-triangle"></i> Usuário ou senha incorretos.</h5>
             </div>';
@@ -49,37 +55,39 @@ class Validation extends Conn {
         endif;
     }
 
-    public static function deslogar() {
-        if (isset($_SESSION['logado'])):
+    public static function deslogar()
+    {
+        if (isset($_SESSION['logado'])) :
             unset($_SESSION['logado']);
             session_destroy();
             echo '<script>window.location.href="../";</script>';
         endif;
     }
 
-    public static function validaSession() {
-        if (!isset($_SESSION['logado'])):
-            $_SESSION['erro'] = 
+    public static function validaSession()
+    {
+        if (!isset($_SESSION['logado'])) :
+            $_SESSION['erro'] =
                 '<div class="alert alert-danger alert-dismissible" role="alert">
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     <h5 align="center"><i class="fas fa-exclamation-triangle"></i> Aréa restrita para usuários cadastrados.</h5>
                 </div>';
             echo '<script>window.location.href="../";</script>';
-        else:
+        else :
             $pdo = parent::getConn();
             $sql = $pdo->prepare("SELECT * FROM usuarios WHERE email = ? AND senha = ?");
             $sql->bindValue(1, $_SESSION['usuario']);
             $sql->bindValue(2, $_SESSION['senha']);
             $sql->execute();
-            if ($sql->rowCount() == 0):
+            if ($sql->rowCount() == 0) :
                 unset($_SESSION['usuario']);
                 unset($_SESSION['senha']);
                 unset($_SESSION['logado']);
                 unset($_SESSION['user']);
-                unset( $_SESSION['userId']);
+                unset($_SESSION['userId']);
                 unset($_SESSION['avatar']);
-                
-                $_SESSION['erro'] = 
+
+                $_SESSION['erro'] =
                     '<div class="alert alert-danger alert-dismissible" role="alert">
                         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                         <h5 align="center"><i class="fas fa-exclamation-triangle"></i> Aréa restrita para usuários cadastrados.</h5>
@@ -89,7 +97,8 @@ class Validation extends Conn {
         endif;
     }
 
-    public static function expiraSession() {
+    public static function expiraSession()
+    {
         if (isset($_SESSION["sessiontime"])) {
             if ($_SESSION["sessiontime"] < time()) {
                 session_unset();
@@ -97,7 +106,7 @@ class Validation extends Conn {
                 unset($_SESSION['senha']);
                 unset($_SESSION['logado']);
                 unset($_SESSION['user']);
-                unset( $_SESSION['tipo']);
+                unset($_SESSION['tipo']);
                 unset($_SESSION['userId']);
                 unset($_SESSION['avatar']);
                 $_SESSION['erro'] = '<div class="alert alert-danger"><i class="fas fa-exclamation-triangle"></i> Sua sessão expirou!</div>';
@@ -111,7 +120,7 @@ class Validation extends Conn {
             unset($_SESSION['senha']);
             unset($_SESSION['logado']);
             unset($_SESSION['user']);
-            unset( $_SESSION['tipo']);
+            unset($_SESSION['tipo']);
             unset($_SESSION['userId']);
             unset($_SESSION['avatar']);
             $_SESSION['erro'] = '<div class="alert alert-danger"><i class="fas fa-exclamation-triangle"></i> Sua sessão expirou!</div>';
@@ -119,20 +128,33 @@ class Validation extends Conn {
         }
     }
 
-    public static function getUser($userId){
+    public static function getUser($userId)
+    {
         $read = new Read();
         $read->ExeRead('usuarios', "where id = $userId");
         return $read->getResult();
     }
 
-    public static function getNameUser($userId){
+    public static function getNameUser($userId)
+    {
         $name = null;
         $read = new Read();
         $read->ExeRead('usuarios', "where id = $userId");
-            foreach($read->getResult() as $user):
-                extract($user);
-                $name = $nome;
-            endforeach;
+        foreach ($read->getResult() as $user) :
+            extract($user);
+            $name = $nome;
+        endforeach;
         return $name;
+    }
+    public static function verificaSeguidor($id_seguindo)
+    {
+        $id_user = $_SESSION['userId'];
+        $read = new Read();
+        $read->ExeRead('seguidores', 'where id_user = ' . $id_user . ' AND id_seguindo = ' . $id_seguindo . '');
+        if ($read->getRowCount() > 0) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- v2/panel/resources/view/dashboard/friends.php
+++ v2/panel/resources/view/dashboard/friends.php
@@ -17,10 +17,16 @@
                             <div class="company-up-info">
                                 <img src="<?php echo $avatar != null ? Url::getBase() . 'uplouds/users/' . $avatar : Url::getBase() . '../public/images/user.png' ?>" alt="<?php echo $avatar ?>" alt="">
                                 <h3><?php echo $nome ?></h3>
+                                <h4><?php echo $email ?></h4>
                                 <h4><?php echo $telefone ?></h4>
                                 <ul>
                                     <li><a href="#" title="Chat" class="message-us"><i class="fa fa-comments"></i></a></li>
-                                    <li><a href="#" title="Seguir" class="hire-us"><i class="fa fa-plus"></i></a></li>
+                                    <?php if (Validation::verificaSeguidor($id)) { ?>
+                                        <li><a href="#" title="deixar de Seguir" class="hire-us seguir" alt="<?php echo $id ?>"><i class="fa fa-minus"></i></a></li>
+                                    <?php } else { ?>
+                                        <li><a href="#" title="Seguir" class="hire-us seguir" alt="<?php echo $id ?>"><i class="fa fa-plus"></i></a></li>
+                                    <?php } ?>
+
                                 </ul>
                             </div>
                         </div>
